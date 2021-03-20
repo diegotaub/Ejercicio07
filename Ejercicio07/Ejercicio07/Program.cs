2704ddeeb7c23e63415d207b8ba09052eb907312
diego.taub <diego.taub@accenture.com>
2021-03-20 18:31:29 -0300

    Ejercicio07

---
+++ Ejercicio07/Ejercicio07/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre: ");
+            Console.Write($"Hola {Console.ReadLine()}");
+            Console.ReadKey();
+        }
+    }
+}
